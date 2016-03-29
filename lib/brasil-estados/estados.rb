c2b9b3e8066fd73c0415217ee28c043968801994
Ricardo Fleury <ricardo.fleury@Centro-200556.centro.net>
2016-03-29 14:04:15 -0500

    Change gem name and structure

---
+++ lib/brasil-estados/estados.rb
@@ -0,0 +1,7 @@
+module Brasil
+	class Estados
+		def self.all
+			['SP', 'RJ']
+		end
+	end
+end
--- lib/geopiece/USA/usa.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../country'
-
-class Usa < Country
-  def self.states
-    ["AK", "AL"]
-  end
-end
